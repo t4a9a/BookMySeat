dd1997f8d9a7a1c08b0dc85dee1b6135b9e53017
t4a9a-kali <thapasinghvaibhav@gmail.com>
2026-03-01 13:54:42 -0500

    added tailwind css and google fonts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,22 +2,26 @@ import { Home } from 'lucide-react'
 import React from 'react'
 import { Routes } from 'react-router-dom'
 import MovieDetails from './pages/MovieDetails'
+import { Toaster } from 'react-hot-toast'
+import Footer from './components/Footer'
 
 const App = () => {
-  
+
   const isAdminRoute = useLocation().pathname.startsWith('/admin')
-  
+
   return (
     <>
-        { !isAdminRoute && <Navbar/> }
-        <Routes>
-          <Route path= '/' element = {<Home/>} />
-          <Route path= '/movies' element = {<Movies/>} />
-          <Route path= '/movies/:id' element = {<MovieDetails/>} />
-          <Route path= '/movies/:id/:date' element = {<SeatLayout/>} />
-          <Route path= '/my-bookings' element = {<MyBookings/>} />
-          <Route path= '/favorite' element = {<Favorite/>} />
-        </Routes>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer/>}
     </>
   )
 }
